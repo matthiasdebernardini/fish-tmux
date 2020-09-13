b981f32c7b6b6c42895b6f6c9fe01557727d4cad
Matthias Debernardini <32387851+matthiasdebernardini@users.noreply.github.com>
2020-09-13 13:53:48 +0200

    Update tmux.fish

--- conf.d/tmux.fish
+++ conf.d/tmux.fish
@@ -1,8 +1,5 @@
-if not command -s tmux > /dev/null
-    echo "tmux: command not found. See https://tmux.github.io/"
-    exit 1
-end
-
-function __tmux_rename_window_on_hook -e fish_prompt -s CONT -v PWD
-    __tmux_rename_window
+if not set -q TMUX
+    set -g TMUX tmux new-session -d -s base
+    eval $TMUX
+    tmux attach-session -d -t base
 end
